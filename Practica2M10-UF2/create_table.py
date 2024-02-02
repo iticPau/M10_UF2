9c211358462dcf7d6a2fa57e592ca25129073dd2
iticPau <2023_pau.insa@iticbcn.cat>
2024-02-02 14:55:55 +0100

    Commit del Jueves intentando solucionar el problema de connection y subir Create

--- Practica2M10-UF2/create_table.py
+++ Practica2M10-UF2/create_table.py
@@ -2,7 +2,7 @@ from conection import conn,cursor
 consulta = '''CREATE TABLE MOTOS(
                 moto_id SERIAL PRIMARY KEY,
                 moto_name VARCHAR(255) NOT NULL,
-                moto_matricula INT NOT NULL,
+                moto_matricula VARCHAR(255) NOT NULL,
                 moto_HP INT NOT NULL
 )'''
 
